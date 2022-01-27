0518193a96336999609f23f5db5ec3bc3544db50
Alexey Kulish <alexkulish1993@gmail.com>
2022-01-27 16:37:05 +0300

    Initial Commit

---
+++ TaskListApp/TaskListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListAppApp.swift
+//  TaskListApp
+//
+//  Created by Alex Kulish on 27.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
